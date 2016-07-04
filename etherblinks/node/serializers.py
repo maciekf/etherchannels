ff737a06fe479b312c26c4dd351094478f564fa6
Maciej Foks <foks.maciej@gmail.com>
2016-07-04 17:10:21 +0200

    deleting unused files

--- etherblinks/node/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class NewChannelSerializer(serializers.Serializer):
-    channel_id = serializers.CharField(max_length=70)
-    from_address = serializers.CharField(max_length=70)
-    to_address = serializers.CharField(max_length=70)
